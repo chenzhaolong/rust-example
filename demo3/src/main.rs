114f793a564359eb75c7a38e1747d9f123e10e68
chenzhaolong <1332588507@qq.com>
2021-05-16 16:27:41 +0800

    demo3

--- demo3/src/main.rs
+++ demo3/src/main.rs
@@ -3,6 +3,9 @@ fn main() {
     func = demo2;
     let _a = func((23, 32.0));
     println!("{}", _a);
+    let b = demo3(3);
+    println!("{}", b);
+    println!("end");
 }
 
 fn demo1 (a: (i32, f32)) -> i32 {
@@ -12,4 +15,26 @@ fn demo1 (a: (i32, f32)) -> i32 {
 fn demo2 (a: (i32, f32)) -> i32 {
    let b = a.1 as i32;
    return a.0 + b;
+}
+
+fn demo3 (x: i32) -> i32 {
+    if x > 20 {
+        demo4();
+    } else {
+        return demo5(x);
+    }
+}
+
+fn demo4 () -> ! {
+    panic!("it is error")
+}
+
+fn demo5(a: i32) -> i32 {
+    if a == 1 {
+        return 1;
+    }
+    if a == 2 {
+        return 2;
+    }
+    return demo5(a - 1) + demo5(a - 2);
 }
