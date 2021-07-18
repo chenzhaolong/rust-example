672eb94c43f6612fbda9255a1c2d999b7fc5a5fb
chenzhaolong <1332588507@qq.com>
2021-07-18 20:11:00 +0800

    rust的数组

--- demo5/src/main.rs
+++ demo5/src/main.rs
@@ -1,3 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    knowArray();
+}
+
+fn knowArray () {
+    // 创建数组
+    let mut a: [i32; 3] = [1,2,3];
+    println!("first: {}", a[1]);
+    // 数组比较
+    let b: [i32; 3] = [1,2, 3];
+    println!("second: {}", b == a);
+    // 获取数组的值
+    println!("third {}", b[0] + a[1]);
+    let c: [i32; 3] = [1,2,5];
+    a = c;
+    for i in &a {
+        println!("{}", i)
+    }
+    let s: &mut [i32; 3] = &mut a;
+    Slice(s);
+    println!("s:{}", s[2]);
+    println!("a:{}", a[2]);
+}
+
+fn Slice(a: &mut [i32]) {
+    a[2] = 5;
+    println!("slice{}", a[2])
+}
+
+fn Fat(a: &[i32]) {
+    println!("Fat{}", a[1]);
 }
